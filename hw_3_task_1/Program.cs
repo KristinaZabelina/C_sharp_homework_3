28fa9ac9ad370851fb72fe6e351382a40a0e94bf
Kristina <zkvzkvzkv@yandex.ru>
2023-01-22 16:08:41 +0300

    создана программа по условию задачи 1

--- hw_3_task_1/Program.cs
+++ hw_3_task_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = 0;
+int t = number;
+
+while (number != 0)
+{
+    int number3 = number % 10;
+    number = number / 10;
+    number2 = number2 * 10 + number3;
+}
+Console.WriteLine(t == number2);
+
